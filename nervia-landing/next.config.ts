e6f616f78d6d02bb077af5c80a7afb9f750e27b3
oholyshevskyi <oleksandr.holyshevskyi@pcmicorp.com>
2026-03-02 13:08:53 +0100

    feat: add experimental turbopack configuration to Next.js setup
    - Introduced experimental turbopack settings in next.config.ts to optimize build performance.
    - Set the root directory for turbopack to the current landing folder, enhancing development efficiency.

--- nervia-landing/next.config.ts
+++ nervia-landing/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      // Вказуємо, що корінь для турбопака — це поточна папка лендінгу
+      root: '.', 
+    },
+  },
 };
 
 export default nextConfig;
